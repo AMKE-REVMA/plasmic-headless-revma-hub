57b937014c35aaeaad0751a6ba784bfd12a404dd
Liam Laidlaw <95653151+lla1dlaw@users.noreply.github.com>
2026-04-01 20:21:53 +0300

    fix: moved plasmic component registration call to the plasmic client provider

--- src/app/plasmic-host/page.tsx
+++ src/app/plasmic-host/page.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from '@/plasmic-init';
+import { PlasmicClientProvider } from '@/app/components/PlasmicClientProvider';
 
 export default function PlasmicHost() {
-  return PLASMIC && <PlasmicCanvasHost />;
+  return (
+    <PlasmicClientProvider>
+      <PlasmicCanvasHost />
+    </PlasmicClientProvider>
+  );
 }
